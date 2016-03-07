556ca00a517822af0c90e55d031e199d791a6a3a
Bruno Rocha <brunos.roch@gmail.com>
2016-03-07 13:33:21 +0100

    Initial Commit

---
+++ eSailors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  eSailors
+//
+//  Created by Bruno Rocha on 3/7/16.
+//  Copyright © 2016 eSailors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
